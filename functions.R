cab2ebbfd9ae78c9574b52bb8228d3b816cecaa0
zstromqvist <zakrisstromqvist@gmail.com>
2020-08-07 13:06:56 +0200

    add base scripts

---
+++ functions.R
@@ -0,0 +1,54 @@
+
+fetch_pbp_data <- function(start_year = 2009, end_year = 2009, path_var = "data/"){
+  
+  reg_pbp <- data.frame()
+  
+  for (year in start_year:end_year) {
+    
+    print(glue("Fetching data for year {year}"))
+    file <- glue("reg_pbp_{year}.csv")
+    path <- glue("{path_var}{file}")
+    
+    tmp_df <- 
+      read_csv(path, col_types = cols(.default = col_character())) %>% 
+      mutate(
+        season = year
+      )
+    
+    reg_pbp <- 
+      reg_pbp %>% 
+      bind_rows(tmp_df)
+  }
+  
+  return(reg_pbp)
+}
+
+calc_qbr <- function(completion_percentage, yards, attempts, tds, ints){
+  
+  a = case_when(
+    ((completion_percentage) - .3) * 5 > 2.375 ~ 2.375,
+    ((completion_percentage) - .3) * 5 < 0 ~ 0,
+    TRUE ~ ((completion_percentage) - .3) * 5
+  )
+  
+  b = case_when(
+    ((yards / attempts) - 3) * .25 > 2.375 ~ 2.375,
+    ((yards / attempts) - 3) * .25 < 0 ~ 0,
+    TRUE ~ ((yards / attempts) - 3) * .25
+  )
+  
+  c = case_when(
+    (tds / attempts) * 20 > 2.375 ~ 2.375,
+    (tds / attempts) * 20 < 0 ~ 0,
+    TRUE ~ (tds / attempts) * 20
+  )
+  
+  d = case_when(
+    2.375 - ((ints / attempts) * 25) > 2.375 ~ 2.375,
+    2.375 - ((ints / attempts) * 25) < 0 ~ 0,
+    TRUE ~ 2.375 - ((ints / attempts) * 25)
+  )
+  
+  return(round(((a + b + c+ d) / (6)) * 100, 1))
+  
+}
---
+++ passing.R
@@ -0,0 +1,152 @@
+library(tidyverse)
+library(readr)
+library(glue)
+library(lubridate)
+
+options(dplyr.summarise.inform = F)
+
+setwd('~/projects/nfl/')
+
+stadiums <- read_csv('stadiums.csv')
+
+reg_pbp <- 
+  fetch_pbp_data(2009, 2019)
+
+pass_df <- 
+  reg_pbp %>% 
+  filter(pass_attempt == 1) %>% 
+  mutate(
+    game_date = date(game_date),
+    game_id = as.numeric(game_id),
+    play_id = as.numeric(play_id),
+    drive = as.numeric(drive),
+    yards_gained = as.numeric(yards_gained),
+    complete_pass = as.numeric(complete_pass),
+    incomplete_pass = as.numeric(incomplete_pass),
+    sack = as.numeric(sack),
+    penalty = as.numeric(penalty),
+    pass_attempt = as.numeric(pass_attempt),
+    touchdown = as.numeric(touchdown),
+    pass_touchdown = as.numeric(pass_touchdown),
+    interception = as.numeric(interception),
+    two_point_attempt = as.numeric(two_point_attempt)
+  ) %>% 
+  select(
+    season,
+    game_date,
+    game_id,
+    play_id,
+    home_team,
+    posteam,
+    posteam_type,
+    drive,
+    desc:yards_after_catch,
+    passer_player_name,
+    complete_pass,
+    incomplete_pass,
+    sack,
+    game_half,
+    penalty,
+    pass_attempt,
+    touchdown,
+    pass_touchdown,
+    interception,
+    two_point_attempt
+    ) %>% 
+  left_join(stadiums, by = c('home_team' = 'team', 'season' = 'season'))
+
+run_df <- 
+  reg_pbp %>% 
+  filter(season == 2019) %>% 
+  filter(rush_attempt == 1) %>% 
+  mutate(
+    game_date = date(game_date),
+    game_id = as.numeric(game_id),
+    play_id = as.numeric(play_id),
+    drive = as.numeric(drive),
+    yards_gained = as.numeric(yards_gained),
+    rush_attempt = as.numeric(rush_attempt),
+    two_point_attempt = as.numeric(two_point_attempt),
+    rush_touchdown = as.numeric(rush_touchdown)
+  ) %>% 
+  select(
+    season,
+    game_date,
+    game_id,
+    play_id,
+    home_team,
+    posteam,
+    posteam_type,
+    drive,
+    rusher_player_name,
+    yards_gained,
+    game_half,
+    penalty,
+    play_type,
+    rush_attempt,
+    touchdown,
+    rush_touchdown,
+    two_point_attempt
+  ) %>% 
+  left_join(stadiums, by = c('home_team' = 'team', 'season' = 'season'))
+
+output <- 
+  pass_df %>% 
+  filter(!game_id %in% c(2013112401, 2013120101)) %>% 
+  filter(sack == 0) %>% 
+  group_by(stadium_type, posteam_type) %>% 
+  summarise(
+    games = length(unique(game_id)),
+    completions = sum(complete_pass),
+    attempts = sum(pass_attempt) - sum(two_point_attempt),
+    compl_perc = round(completions / attempts, 3),
+    yards = sum(yards_gained),
+    tds = sum(pass_touchdown),
+    ints = sum(interception)
+  ) %>% 
+  ungroup() %>% 
+  mutate(
+    yards_per_game = round(yards / games),
+    tds_per_game = round(tds / games, 2),
+    td_int_ratio = round(tds / ints, 2),
+    qbr = calc_qbr(compl_perc, yards, attempts, tds, ints)
+  ) %>% 
+  arrange(desc(qbr))
+
+qb_stats <- 
+  pass_df %>% 
+  filter(!game_id %in% c(2013112401, 2013120101)) %>% 
+  filter(sack == 0) %>% 
+  filter(season == 2018) %>% 
+  group_by(passer_player_name) %>% 
+  summarise(
+    games = length(unique(game_id)),
+    completions = sum(complete_pass),
+    attempts = sum(pass_attempt) - sum(two_point_attempt),
+    compl_perc = round(completions / attempts, 3),
+    yards = sum(yards_gained),
+    tds = sum(pass_touchdown),
+    ints = sum(interception)
+  ) %>% 
+  ungroup() %>% 
+  mutate(
+    yards_per_game = round(yards / games),
+    tds_per_game = round(tds / games, 2),
+    td_int_ratio = round(tds / ints, 2),
+    qbr = calc_qbr(compl_perc, yards, attempts, tds, ints)
+  ) %>% 
+  select(
+    -c(a,b,c,d)
+  ) %>% 
+  arrange(desc(yards))
+
+run_output <- 
+  run_df %>% 
+  group_by(rusher_player_name) %>% 
+  summarise(
+    games = length(unique(game_id)),
+    attempts = sum(rush_attempt) - sum(two_point_attempt),
+    yards = sum(yards_gained),
+    tds = sum(rush_touchdown)
+  ) %>% 
+  arrange(desc(yards))
